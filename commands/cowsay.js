ba58ab4a5051363a39db1c298dd8665d7ffec909
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-12-18 18:08:16 -0500

    Cowsay Testing

--- commands/cowsay.js
+++ commands/cowsay.js
@@ -7,26 +7,40 @@ module.exports = {
     .setDescription('Makes a cow message.')
     .addStringOption((option) => option.setName('message')
       .setDescription('Contents of the cow message')
-      .setMaxLength(100)
+      .setMaxLength(25)
       .setRequired(true))
     // unsure if message could be made optional? tried messing with it and couldn't get it to work
     .addStringOption((option) => option.setName('type')
       .setDescription("Type of 'cow'")
-      .setMaxLength(100)
+      .setMaxLength(25)
       .setRequired(false)),
   async execute(interaction) {
     const cowMessage = interaction.options.getString('message');
     const cowType = interaction.options.getString('type');
-    const cow = cowsay.say({
-      text: cowMessage,
-      f: cowType,
-    }).replaceAll('`', "'");
-    console.log(cow.length);
-    // ensures total character length <2000 (reply formatting adds 6 characters)
-    if (cow.length < 1994) {
-      await interaction.reply(`\`\`\`${cow}\`\`\``);
+    const cowList = await cowsay.list((error, cowNames) => {
+      if (error) {
+        console.log(error);
+      } else if (cowNames) {
+        console.log(`Number of cows available: ${cowNames.length}`);
+      }
+    });
+    // console.log(cowType);
+    // console.log(`${cowType}.cow`);
+    // console.log(cowList);
+    if (cowList.includes(`${cowType}.cow`)) {
+      const cow = cowsay.say({
+        text: cowMessage,
+        f: cowType,
+      }).replaceAll('`', "'");
+      console.log(cowsay.list());
+      // ensures total character length <2000 (reply formatting adds 6 characters)
+      if (cow.length < 1994) {
+        await interaction.reply(`\`\`\`${cow}\`\`\``);
+      } else {
+        await interaction.reply('Your Message Exceeded The Character Limit!');
+      }
     } else {
-      await interaction.reply('Your Message Exceeded The Character Limit!');
+      await interaction.reply('Invalid Cow Type!');
     }
   },
 };
