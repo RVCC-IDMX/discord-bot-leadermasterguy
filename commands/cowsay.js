0b764903b94be0e177e0308dbbcbf4d941de0583
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-12-18 17:29:36 -0500

    Cowsay Text and Type input

--- commands/cowsay.js
+++ commands/cowsay.js
@@ -4,11 +4,29 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('cowsay')
-    .setDescription('Makes a cow message.'),
+    .setDescription('Makes a cow message.')
+    .addStringOption((option) => option.setName('message')
+      .setDescription('Contents of the cow message')
+      .setMaxLength(100)
+      .setRequired(true))
+    // unsure if message could be made optional? tried messing with it and couldn't get it to work
+    .addStringOption((option) => option.setName('type')
+      .setDescription("Type of 'cow'")
+      .setMaxLength(100)
+      .setRequired(false)),
   async execute(interaction) {
+    const cowMessage = interaction.options.getString('message');
+    const cowType = interaction.options.getString('type');
     const cow = cowsay.say({
-      text: 'Moo',
+      text: cowMessage,
+      f: cowType,
     }).replaceAll('`', "'");
-    await interaction.reply(`\`\`\`${cow}\`\`\``);
+    console.log(cow.length);
+    // ensures total character length <2000 (reply formatting adds 6 characters)
+    if (cow.length < 1994) {
+      await interaction.reply(`\`\`\`${cow}\`\`\``);
+    } else {
+      await interaction.reply('Your Message Exceeded The Character Limit!');
+    }
   },
 };
