aa4792ea7fe35d1833c858d73317582462d95d2b
Nicholas Trilone <leadermasterguy2@gmail.com>
2022-12-18 18:38:32 -0500

    Cowsay Input Functions

--- commands/cowsay.js
+++ commands/cowsay.js
@@ -1,15 +1,22 @@
 const cowsay = require('cowsay');
 const { SlashCommandBuilder } = require('discord.js');
 
+function getCows(error, cowNames) {
+  if (error) {
+    console.log(error);
+  } else if (cowNames) {
+    console.log(`Number of cows available: ${cowNames.length}`);
+  }
+}
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('cowsay')
     .setDescription('Makes a cow message.')
     .addStringOption((option) => option.setName('message')
       .setDescription('Contents of the cow message')
-      .setMaxLength(25)
+      .setMaxLength(200)
       .setRequired(true))
-    // unsure if message could be made optional? tried messing with it and couldn't get it to work
     .addStringOption((option) => option.setName('type')
       .setDescription("Type of 'cow'")
       .setMaxLength(25)
@@ -17,13 +24,7 @@ module.exports = {
   async execute(interaction) {
     const cowMessage = interaction.options.getString('message');
     const cowType = interaction.options.getString('type');
-    const cowList = await cowsay.list((error, cowNames) => {
-      if (error) {
-        console.log(error);
-      } else if (cowNames) {
-        console.log(`Number of cows available: ${cowNames.length}`);
-      }
-    });
+    const cowList = await cowsay.list(getCows);
     // console.log(cowType);
     // console.log(`${cowType}.cow`);
     // console.log(cowList);
@@ -32,7 +33,7 @@ module.exports = {
         text: cowMessage,
         f: cowType,
       }).replaceAll('`', "'");
-      console.log(cowsay.list());
+      console.log(cowsay.list(getCows));
       // ensures total character length <2000 (reply formatting adds 6 characters)
       if (cow.length < 1994) {
         await interaction.reply(`\`\`\`${cow}\`\`\``);
